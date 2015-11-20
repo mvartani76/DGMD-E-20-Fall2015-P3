5ab482e251632d0b770aa4fc4c16cd6f2edd7fc2
mvartani76 <mike.vartanian@gmail.com>
2015-11-20 14:25:33 -0500

    Removed footer.css link as there is no footer.css and changed username/password ids to be page specific so the jquery validation does not trigger here...

--- login.php
+++ login.php
@@ -5,7 +5,6 @@
   <link href="./css/menu.css" rel="stylesheet">
   <link href="./css/formstyle.css" rel="stylesheet">
   <link href="./css/services.css" rel="stylesheet">
-  <link href="./css/footer.css" rel="stylesheet">  
   <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>    
 </head>
 <body>
@@ -31,8 +30,8 @@
 		<form id="login_id" method="post">
 			<h3>Login</h3>
     		<h4>Please enter your username and password to login...</h4>
-    		<fieldset><input placeholder="Username" id="username" name="username" type="text" tabindex="1" required autofocus></fieldset>
-    		<fieldset><input placeholder="Password" id="userpassword" name="userpassword" type="password" tabindex="2" required></fieldset>
+    		<fieldset><input placeholder="Username" id="login_username" name="username" type="text" tabindex="1" required autofocus></fieldset>
+    		<fieldset><input placeholder="Password" id="login_userpassword" name="userpassword" type="password" tabindex="2" required></fieldset>
     		<fieldset>
       		<button name="submit" type="submit" id="login_id-submit" data-submit="...Sending">Submit</button>
     		</fieldset>
